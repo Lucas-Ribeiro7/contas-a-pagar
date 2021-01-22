615fdb84499e62723dc8c66dd6b49a75d3547aca
Lucas Ribeiro <caetanolucasribeiro@gmail.com>
2021-01-22 15:46:22 -0300

    Update

--- cadastro.php
+++ cadastro.php
@@ -1,20 +1,5 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="estilo.css">
-    <title>Contas a Pagar</title>
-</head>
-<body>
-    <div class="menu">
-        <a href="index.php">HOME</a> |
-        <a href="dados.php">CONTAS A PAGAR</a> |
-        <a href="cadastro.php">CADASTRAMENTO DE CONTAS</a> |
-        <a href="pagas.php">CONTAS PAGAS</a> |
-        <a href="procedimentos.php">PROCEDIMENTOS</a>
-    </div>
-    <br>
+    <?php include "cabecalho.php"; ?>
+
     <div class="cadastro">
         <form action="processa.php" method="POST">
             <p>Código para identificação:<input type="number" name="codigo" required></p>
