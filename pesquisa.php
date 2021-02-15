8ca803de146b62176595cf2a0e32a85b4ad2fcad
Lucas Ribeiro <caetanolucasribeiro@gmail.com>
2021-02-15 18:05:44 -0300

    update

--- pesquisa.php
+++ pesquisa.php
@@ -18,34 +18,38 @@
                 ?>
                 <h2>Sua Pesquisa:</h2>
                 <table>
-                <tr>
-                    <td><b>CODIGO</b></td>
-                    <td><b>DESCRIÇÃO</b></td>
-                    <td><b>VALOR</b></td>
-                    <td><b>DATA DE VENCIMENTO</b></td>
-                    <td><b>SITUAÇÃO</b></td>
-                </tr>
-                    <?php
-                    
-                    if($cliente == 0){
-                        echo "<h2>Não encontramos nenhuma conta</h2>";
-                    }else{
-                        if($cliente["0"]["situacao"] == 1){
-                            $cliente["0"]["situacao"] = "EM ABERTO";
-                        }else{
-                            $cliente["0"]["situacao"] = "PAGO";
-                        }
-                    
-                        for($i = 0; $i < count($cliente); $i++){
-                            echo "<tr>";
-                            foreach($cliente[$i] as $key => $value){
-                                    echo "<td>$value</td>";
+                    <tr>
+                        <td><b>CODIGO</b></td>
+                        <td><b>DESCRIÇÃO</b></td>
+                        <td><b>VALOR</b></td>
+                        <td><b>DATA DE VENCIMENTO</b></td>
+                        <td><b>SITUAÇÃO</b></td>
+                        <td colspan="2"> <b>ALTERAR</b> </td>
+                    </tr>
+                        <?php                        
+                            if($cliente == 0){
+                                echo "<h2>Não encontramos nenhuma conta</h2>";
+                            }else{
+                                if($cliente["0"]["situacao"] == 1){
+                                    $cliente["0"]["situacao"] = "EM ABERTO";
+                                }else{
+                                    $cliente["0"]["situacao"] = "PAGO";
+                                }
+                            
+                                for($i = 0; $i < count($cliente); $i++){
+                                    echo "<tr>";
+                                    foreach($cliente[$i] as $key => $value){
+                                            echo "<td>$value</td>";
+                                    }
+                                    ?>
+                                    <td> <a href="editar.php?codigo=<?php echo $cliente[$i]["codigo"]; ?>" class="editar"> EDITAR </a> </td>
+                                    <td> <a href="dados.php?codigo=<?php echo $cliente[$i]["codigo"]; ?>" class="excluir"> EXCLUIR </a> </td>
+                                    <?php
+                                    echo "</tr>";
+                                }
                             }
-                            echo "</tr>";
                         }
-                    }
-                }
-                    ?>
+                        ?>
                 </table>
             </div>
         </div>
